ef3daf8b66c85a233427de9dcea6020f86d3140e
pbpbpb2705 <phambao2705@gmail.com>
2024-05-20 17:48:02 +1000

    Add project files.

---
+++ Dish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Dish : PayableComponent
+    {
+        public Dish(string name, decimal price) : base(name, price)
+        {
+        }
+    }
+}
---
+++ PayableComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal abstract class PayableComponent
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public PayableComponent(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public virtual decimal CalculatePayment(int count)
+        {
+            return Price * count;
+        }
+    }
+}
